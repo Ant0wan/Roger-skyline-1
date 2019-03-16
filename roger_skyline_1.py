c7cdf050e8587672cba9fe75fa6426d878a4d3bd
Ant0wan <abarthel@student.42.fr>
2019-03-16 10:14:39 -0400

    automatise VM deployment

--- roger_skyline_1.py
+++ roger_skyline_1.py
@@ -13,22 +13,26 @@
 
 import os
 
-# Get the path
+## Get the path
 path = os.getcwd()
 
-# Virtual Machine Info
+## Virtual Machine Info
 VM_name = "MiniDebian"
 VM_type = "Debian_64"
 cpu = 4
 RAM = 4096
 NAT2 = "en0"
 
-# Get the Debian iso
+## Get the Debian iso
 os.system("curl -O http://ftp.nl.debian.org/debian/dists/stretch/main/installer-amd64/current/images/netboot/mini.iso")
 
-#print ("shasum < disk.vdi")
+##print ("shasum < disk.vdi")
 os.system("VBoxManage createvm --name MiniDebian --ostype Debian_64 --register")
 os.system("VBoxManage modifyvm " + VM_name + " --cpus " + str(cpu) + " --memory " + str(RAM))
 os.system("VBoxManage modifyvm " + VM_name + " --nic2 bridged  --bridgeadapter1 " + NAT2)
+os.system("VBoxManage storagectl " + VM_name + " --name IDE --add ide")
+os.system("VBoxManage storageattach " + VM_name + " --storagectl IDE --port 0 --device 0 --type dvddrive --medium mini.iso")
 
-print ("https://www.oracle.com/technetwork/articles/servers-storage-admin/manage-vbox-cli-2264359.html")
+
+#vboxmanage unregistervm MiniDebian
+#print ("https://www.oracle.com/technetwork/articles/servers-storage-admin/manage-vbox-cli-2264359.html")
