3aad9c544a93f898a5b83d68314d7e922d160732
Ant0wan <abarthel@student.42.fr>
2019-03-16 11:50:22 -0400

    added some commands

--- roger_skyline_1.py
+++ roger_skyline_1.py
@@ -19,8 +19,8 @@ path = os.getcwd()
 ## Virtual Machine Info
 VM_name = "MiniDebian"
 VM_type = "Debian_64"
-cpu = 4
-RAM = 4096
+cpu = 1
+RAM = 1024
 NAT2 = "en0"
 
 ## Get the Debian iso
@@ -29,10 +29,15 @@ os.system("curl -O http://ftp.nl.debian.org/debian/dists/stretch/main/installer-
 ##print ("shasum < disk.vdi")
 os.system("VBoxManage createvm --name MiniDebian --ostype Debian_64 --register")
 os.system("VBoxManage modifyvm " + VM_name + " --cpus " + str(cpu) + " --memory " + str(RAM))
-os.system("VBoxManage modifyvm " + VM_name + " --nic2 bridged  --bridgeadapter1 " + NAT2)
+#BUG #os.system("VBoxManage modifyvm " + VM_name + " --nic2 bridged  --bridgeadapter1 " + NAT2)
 os.system("VBoxManage storagectl " + VM_name + " --name IDE --add ide")
 os.system("VBoxManage storageattach " + VM_name + " --storagectl IDE --port 0 --device 0 --type dvddrive --medium mini.iso")
-os.system("VBoxManage storageattach " + VM_name + " --storagectl IDE --port 0 --device 0 --medium mini.iso")
+#os.system("VBoxManage storageattach " + VM_name + " --storagectl IDE --port 0 --device 0 --medium none")
 
-#vboxmanage unregistervm MiniDebian
+## Start the VM
+#os.system("vboxmanage startvm " + VM_name + " --type headless")
+
+## Delete the VM
+#os.system("vboxmanage shutdown " + VM_name)
+#os.system("vboxmanage unregistervm " + VM_name + " --delete")
 #print ("https://www.oracle.com/technetwork/articles/servers-storage-admin/manage-vbox-cli-2264359.html")
