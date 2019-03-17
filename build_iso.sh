626a83e08a9fa641d3e9c490a072817c1bd40170
Ant0wan <abarthel@student.42.fr>
2019-03-17 09:34:42 -0400

    done clean

--- build_iso.sh
+++ build_iso.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
 
 ## Adding the preseed.cfg file to Initrd
-mkdir DESTINATION
-bsdtar -C DESTINATION -xf preseed-mini.iso
+mkdir ./DESTINATION/
+bsdtar -C ./DESTINATION/ -xf mini.iso
+chmod +w -R ./DESTINATION/
 cd ./DESTINATION/
-chmod +w initrd.gz
 gunzip initrd.gz
-echo preseed.cfg | cpio -H newc -o -A -F initrd
+echo ../preseed.cfg | cpio -H newc -o -A -F initrd
 gzip initrd
-chmod -w initrd
+cd ..
+chmod -w -R ./DESTINATION/
+cd ./DESTINATION/
 md5sum `find -follow -type f` > md5sum.txt
+cd ..
 genisoimage -r -J -b isolinux.bin -c boot.cat -no-emul-boot -boot-load-size 4 -boot-info-table -o preseed-mini.iso ./DESTINATION/
