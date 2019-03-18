dd4c9c47b29f3d70e9dc70b6edda3c972988db27
Ant0wan <abarthel@student.42.fr>
2019-03-18 11:20:35 +0100

    adding script py including the img building

--- roger_skyline_1.py
+++ roger_skyline_1.py
@@ -19,19 +19,42 @@ path = os.getcwd()
 ## Virtual Machine Info
 VM_name = "MiniDebian"
 VM_type = "Debian_64"
-cpu = 1
-RAM = 1024
+cpu = 2
+RAM = 2048
 NAT2 = "en0"
 
 ## Get the Debian iso
 os.system("curl -O http://ftp.nl.debian.org/debian/dists/stretch/main/installer-amd64/current/images/netboot/mini.iso")
 
-##print ("shasum < disk.vdi")
+
+## Adding the preseed.cfg file to Initrd
+#os.system("./build_iso.sh")
+os.system("mkdir ./DESTINATION/")
+os.system("bsdtar -C ./DESTINATION/ -xf mini.iso")
+os.system("chmod +w -R ./DESTINATION/")
+os.system("cd ./DESTINATION/")
+os.system("gunzip initrd.gz")
+os.system("echo ../preseed.cfg | cpio -H newc -o -A -F initrd")
+os.system("gzip initrd")
+os.system("cp -f ../isolinux.cfg ./")
+os.system("cd ..")
+os.system("chmod -w -R ./DESTINATION/")
+os.system("cd ./DESTINATION/")
+os.system("md5sum `find -follow -type f` > md5sum.txt")
+os.system("cd ..")
+os.system("genisoimage -r -J -b isolinux.bin -c boot.cat -no-emul-boot -boot-load-size 4 -boot-info-table -o preseed-mini.iso ./DESTINATION/")
+os.system("rm -rf ./DESTINATION/")
+
+## Create and configure the VM
 os.system("VBoxManage createvm --name MiniDebian --ostype Debian_64 --register")
 os.system("VBoxManage modifyvm " + VM_name + " --cpus " + str(cpu) + " --memory " + str(RAM))
 #BUG #os.system("VBoxManage modifyvm " + VM_name + " --nic2 bridged  --bridgeadapter1 " + NAT2)
 os.system("VBoxManage storagectl " + VM_name + " --name IDE --add ide")
+
+## Mount the iso on the VM
 os.system("VBoxManage storageattach " + VM_name + " --storagectl IDE --port 0 --device 0 --type dvddrive --medium mini.iso")
+
+## Unmount the iso from the VM
 #os.system("VBoxManage storageattach " + VM_name + " --storagectl IDE --port 0 --device 0 --medium none")
 
 ## Start the VM
@@ -41,3 +64,4 @@ os.system("VBoxManage storageattach " + VM_name + " --storagectl IDE --port 0 --
 #os.system("vboxmanage shutdown " + VM_name)
 #os.system("vboxmanage unregistervm " + VM_name + " --delete")
 #print ("https://www.oracle.com/technetwork/articles/servers-storage-admin/manage-vbox-cli-2264359.html")
+##print ("shasum < disk.vdi")
