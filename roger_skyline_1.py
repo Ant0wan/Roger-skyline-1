24101150be6115cb9d4e428ea9597ffd88a710a8
Ant0w@n <43214931+Ant0wan@users.noreply.github.com>
2019-03-22 22:18:38 +0100

    Update roger_skyline_1.py
    Going for a script using functions and a main for commenting whole bunch of functions if needed

--- roger_skyline_1.py
+++ roger_skyline_1.py
@@ -20,44 +20,54 @@ path = os.getcwd()
 VM_name = "MiniDebian"
 VM_type = "Debian_64"
 cpu = 2
-RAM = 2048
+ram = 2048
+disk_size = 8000
 NAT2 = "en0"
 
-## Get the Debian iso
-#os.system("curl -O http://ftp.nl.debian.org/debian/dists/stretch/main/installer-amd64/current/images/netboot/mini.iso")
-
+## Get the Debian iso()
+def download_iso()
+    os.system("curl -O http://ftp.nl.debian.org/debian/dists/stretch/main/installer-amd64/current/images/netboot/mini.iso")
 
 ## Adding the preseed.cfg file to Initrd
-#os.system("./build_iso.sh")
+def launch_build()
+    os.system("./build_iso.sh")
 
 ## Create and configure the VM
-os.system("VBoxManage createvm --name MiniDebian --ostype Debian_64 --register")
-os.system("VBoxManage modifyvm " + VM_name + " --cpus " + str(cpu) + " --memory " + str(RAM))
-#BUG#os.system("VBoxManage modifyvm " + VM_name + " --nic2 bridged  --bridgeadapter1 " + NAT2)
-os.system("VBoxManage storagectl " + VM_name + " --name IDE --add ide")
-os.system("VBoxManage storagectl " + VM_name + " --name SATA --add SATA")
+def configure_vm(VM_name, cpu, ram)
+    os.system("VBoxManage createvm --name MiniDebian --ostype Debian_64 --register")
+    os.system("VBoxManage modifyvm " + VM_name + " --cpus " + str(cpu) + " --memory " + str(ram))
+    #BUG#os.system("VBoxManage modifyvm " + VM_name + " --nic2 bridged  --bridgeadapter1 " + NAT2)
 
 ## Mount the iso on the VM
-os.system("VBoxManage storageattach " + VM_name + " --storagectl IDE --port 0 --device 0 --type dvddrive --medium preseed-mini.iso")
-
 ## Create and mount the disk vdi
-os.system("vboxmanage createmedium disk --filename diskdebianmini --size 8000 --format VDI")
-os.system("VBoxManage storageattach " + VM_name + " --storagectl SATA --port 0 --device 0 --type hdd --medium diskdebianmini.vdi")
-#OLD#os.system("VBoxManage storagectl " + VM_name + " --name diskdebianmini.vdi --add ide --bootable on")
+def create_disk(VM_name, disk_size):
+    os.system("VBoxManage storagectl " + VM_name + " --name IDE --add ide")
+    os.system("VBoxManage storageattach " + VM_name + " --storagectl IDE --port 0 --device 0 --type dvddrive --medium preseed-mini.iso")
+    os.system("VBoxManage storagectl " + VM_name + " --name SATA --add SATA")
+    os.system("vboxmanage createmedium disk --filename diskdebianmini --size " + disk_size + " --format VDI")
+    os.system("VBoxManage storageattach " + VM_name + " --storagectl SATA --port 0 --device 0 --type hdd --medium diskdebianmini.vdi")
 
 ## Boot order
-os.system("vboxmanage modifyvm " + VM_name + " --boot1 disk")
-os.system("vboxmanage modifyvm " + VM_name + " --boot2 dvd")
-os.system("vboxmanage modifyvm " + VM_name + " --boot3 none")
+def boot_order(VM_name):
+    os.system("vboxmanage modifyvm " + VM_name + " --boot1 disk")
+    os.system("vboxmanage modifyvm " + VM_name + " --boot2 dvd")
+    os.system("vboxmanage modifyvm " + VM_name + " --boot3 none")
 
 ## Unmount the iso from the VM
-#os.system("VBoxManage storageattach " + VM_name + " --storagectl IDE --port 0 --device 0 --medium none")
+def unmount_iso(VM_name)
+    os.system("VBoxManage storageattach " + VM_name + " --storagectl IDE --port 0 --device 0 --medium none")
 
 ## Start the VM
-#os.system("vboxmanage startvm " + VM_name + " --type headless")
+def start_vm(VM_name)
+    os.system("vboxmanage startvm " + VM_name + " --type headless")
 
 ## Delete the VM
-#os.system("vboxmanage shutdown " + VM_name)
-#os.system("vboxmanage unregistervm " + VM_name + " --delete")
+def shutdown_vm(VM_name)
+    os.system("vboxmanage shutdown " + VM_name)
+
+def delete_vm(VM_name)
+    os.system("vboxmanage unregistervm " + VM_name + " --delete")
+
+
 #print ("https://www.oracle.com/technetwork/articles/servers-storage-admin/manage-vbox-cli-2264359.html")
 ##print ("shasum < disk.vdi")
