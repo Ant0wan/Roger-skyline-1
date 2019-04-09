d85545c00b45b6354318a02f0ab150262b12f4b9
Ant0wan <abarthel@student.42.fr>
2019-04-09 15:08:46 -0400

    pressed with down VM

--- iso/build_iso.sh
+++ iso/build_iso.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
 ## Adding the preseed.cfg file to Initrd
+apt-get install -y curl
+apt-get install -y bsdtar
+apt-get install -y gzip
+apt-get install -y genisoimage
 curl -O http://ftp.nl.debian.org/debian/dists/stretch/main/installer-amd64/current/images/netboot/mini.iso
 mkdir ./DESTINATION/
 bsdtar -C ./DESTINATION/ -xf mini.iso
@@ -16,4 +20,4 @@ cd ./DESTINATION/
 md5sum `find -follow -type f` > md5sum.txt
 cd ..
 genisoimage -r -J -b isolinux.bin -c boot.cat -no-emul-boot -boot-load-size 4 -boot-info-table -o preseed-mini.iso ./DESTINATION/
-rm -rf ./DESTINATION/
+rm -rf ./DESTINATION/ mini.iso
