aa5d886a0c5c48ec382fb9c2e45c79076a953f11
Ant0w@n <43214931+Ant0wan@users.noreply.github.com>
2019-04-10 23:12:00 +0200

    Update roger_skyline_1.py

--- roger_skyline_1.py
+++ roger_skyline_1.py
@@ -15,12 +15,12 @@ import sys
 from os import system, getcwd
 
 ## Get the path
-path = os.getcwd()
+path = getcwd()
 
 ## Create and configure the VM
 def configure_vm(VM_name, cpu, ram, OS_type, disk_size):
-    os.system("VBoxManage createvm --name " + VM_name + " --ostype " + OS_type + " --register")
-    os.system("VBoxManage modifyvm " + VM_name + " --cpus " + cpu + " --memory " + ram)
+    system("VBoxManage createvm --name " + VM_name + " --ostype " + OS_type + " --register")
+    system("VBoxManage modifyvm " + VM_name + " --cpus " + cpu + " --memory " + ram)
     create_disk(VM_name, disk_size)
     boot_order(VM_name)
     #BUG#os.system("VBoxManage modifyvm " + VM_name + " --nic2 bridged  --bridgeadapter1 " + NAT2)
@@ -33,16 +33,16 @@ def create_disk(VM_name, disk_size):
     disk_file_format = 'vdi'
     disk_type = 'hdd'
     for a_type in storage:
-        os.system("VBoxManage storagectl " + VM_name + " --name " + a_type + " --add " + a_type)
-    os.system("VBoxManage storageattach " + VM_name + " --storagectl " + storage[0] + " --port 0 --device 0 --type dvddrive --medium preseed-mini.iso")
-    os.system("vboxmanage createmedium disk --filename " + disk_name + " --size " + disk_size + " --format " + disk_file_format)
-    os.system("VBoxManage storageattach " + VM_name + " --storagectl " + storage[1] + " --port 0 --device 0 --type " + disk_type + " --medium " + disk_name + "." + disk_file_format)
+        system("VBoxManage storagectl " + VM_name + " --name " + a_type + " --add " + a_type)
+    system("VBoxManage storageattach " + VM_name + " --storagectl " + storage[0] + " --port 0 --device 0 --type dvddrive --medium preseed-mini.iso")
+    system("vboxmanage createmedium disk --filename " + disk_name + " --size " + disk_size + " --format " + disk_file_format)
+    system("VBoxManage storageattach " + VM_name + " --storagectl " + storage[1] + " --port 0 --device 0 --type " + disk_type + " --medium " + disk_name + "." + disk_file_format)
 
 ## Boot order
 def boot_order(VM_name):
     boot_devices = ('disk', 'dvd', 'none')
     for device in boot_devices:
-        os.system("vboxmanage modifyvm " + VM_name + " --boot"(boot_devices.index(device) + 1) + " " + device)
+        system("vboxmanage modifyvm " + VM_name + " --boot"(boot_devices.index(device) + 1) + " " + device)
 
 '''
 ## Unmount the iso from the VM
@@ -51,14 +51,14 @@ def unmount_iso(VM_name):
 '''
 ## Start the VM
 def start_vm(VM_name):
-    os.system("vboxmanage startvm " + VM_name + " --type headless")
+    system("vboxmanage startvm " + VM_name + " --type headless")
 
 ## Delete the VM
 def shutdown_vm(VM_name):
-    os.system("vboxmanage shutdown " + VM_name)
+    system("vboxmanage shutdown " + VM_name)
 
 def delete_vm(VM_name):
-    os.system("vboxmanage unregistervm " + VM_name + " --delete")
+    system("vboxmanage unregistervm " + VM_name + " --delete")
 
 def main(arg):
     ## Virtual Machine Info  
