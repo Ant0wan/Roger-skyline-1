1216f90ed09bb125cee9c3a0d8da7e8fc990bae1
Ant0wan <abarthel@student.42.fr>
2019-04-12 15:06:06 +0200

    adding setup file

---
+++ setup.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+# **************************************************************************** #
+#                                                                              #
+#                                                         :::      ::::::::    #
+#    setup.py                                           :+:      :+:    :+:    #
+#                                                     +:+ +:+         +:+      #
+#    By: abarthel <abarthel@student.42.fr>          +#+  +:+       +#+         #
+#                                                 +#+#+#+#+#+   +#+            #
+#    Created: 2019/04/12 14:40:52 by abarthel          #+#    #+#              #
+#    Updated: 2019/04/12 14:40:52 by abarthel         ###   ########.fr        #
+#                                                                              #
+# **************************************************************************** #
+
+from setuptools import setup, find_packages
+from os import system, getcwd
+import sys
+
+with open('README.md') as f:
+    readme = f.read()
+
+setup(
+    name='roger-skyline-1',
+    version='0.0.1',
+    description='Script for VirtualBox virtual machine and Apache web server \
+                deployment automation. [a 42 project]',
+    long_description=readme,
+    author='Antoine Barthelemy',
+    author_email='abarthel@student.42.fr',
+    url='https://github.com/Ant0wan/Roger-skyline-1.git',
+    license='GPL-3.0-or-later',
+    packages=find_packages(exclude=('tests', 'docs'))
+)
