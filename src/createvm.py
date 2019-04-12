8d3b543a0d4dcdf6a44472e6d4004228602e06ff
Ant0wan <abarthel@student.42.fr>
2019-04-12 19:05:09 +0200

    ok for boot order

--- src/createvm.py
+++ src/createvm.py
@@ -19,11 +19,11 @@ def configure_vm(dinfo):
     system("vboxmanage modifyvm " + dinfo['VM_name'] \
             + " --cpus " + dinfo['cpu'] \
             + " --memory " + dinfo['ram'])
-    create_disk(dinfo['VM_name'], dinfo['disk_size'])
+    create_disk(dinfo['VM_name'], dinfo['disk_size'], dinfo['iso_path'])
     boot_order(dinfo['VM_name'])
 #BUG#    system("VBoxManage modifyvm " + VM_name + " --nic2 bridged  --bridgeadapter1 " + NAT2)
 
-def create_disk(VM_name, disk_size):
+def create_disk(VM_name, disk_size, iso_path):
     storage = ('IDE', 'SATA')
     disk_name = 'diskdebianmini'
     disk_file_format = 'vdi'
@@ -33,16 +33,22 @@ def create_disk(VM_name, disk_size):
                 + " --name " + a_type + " --add " + a_type)
     system("VBoxManage storageattach " + VM_name \
             + " --storagectl " + storage[0] \
-            + " --port 0 --device 0 --type dvddrive --medium ./iso/preseed-mini.iso")
-    system("vboxmanage createmedium disk --filename " + disk_name + " --size " + disk_size + " --format " + disk_file_format)
-    system("VBoxManage storageattach " + VM_name + " --storagectl " + storage[1] + " --port 0 --device 0 --type " + disk_type + " --medium " + disk_name + "." + disk_file_format)
+            + " --port 0 --device 0 --type dvddrive" \
+            + " --medium " + iso_path)
+    system("vboxmanage createmedium disk --filename " + disk_name \
+            + " --size " + disk_size + " --format " + disk_file_format)
+    system("VBoxManage storageattach " + VM_name \
+            + " --storagectl " + storage[1] \
+            + " --port 0 --device 0 --type " + disk_type \
+            + " --medium " + disk_name + "." + disk_file_format)
 
 ## Boot order
 def boot_order(VM_name):
     boot_devices = ('disk', 'dvd', 'none')
     for device in boot_devices:
-        print(boot_devices.index(device) + 1)
-#        system("vboxmanage modifyvm " + VM_name + " --boot"(boot_devices.index(device) + 1) + " " + device)
+        system("vboxmanage modifyvm " + VM_name \
+                + " --boot" + str(boot_devices.index(device) + 1) \
+                + " " + device)
 
 '''
 ## Unmount the iso from the VM
