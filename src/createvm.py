9562e027fe1edbc56a640d17381e8d4fff5a3bf3
Ant0wan <abarthel@student.42.fr>
2019-04-29 16:59:05 +0200

    push

--- src/createvm.py
+++ src/createvm.py
@@ -19,11 +19,11 @@ def configure_vm(dinfo):
     system("vboxmanage modifyvm " + dinfo['VM_name'] \
             + " --cpus " + dinfo['cpu'] \
             + " --memory " + dinfo['ram'])
-    create_disk(dinfo['VM_name'], dinfo['disk_size'], dinfo['iso_path'])
+    create_disk(dinfo['VM_name'], dinfo['disk_size'], dinfo['VDI_path'], dinfo['iso_path'])
     boot_order(dinfo['VM_name'])
 #BUG#    system("VBoxManage modifyvm " + VM_name + " --nic2 bridged  --bridgeadapter1 " + NAT2)
 
-def create_disk(VM_name, disk_size, iso_path):
+def create_disk(VM_name, disk_size, disk_path, iso_path):
     storage = ('IDE', 'SATA')
     disk_name = 'diskdebianmini'
     disk_file_format = 'vdi'
@@ -40,7 +40,7 @@ def create_disk(VM_name, disk_size, iso_path):
     system("VBoxManage storageattach " + VM_name \
             + " --storagectl " + storage[1] \
             + " --port 0 --device 0 --type " + disk_type \
-            + " --medium " + disk_name + "." + disk_file_format)
+            + " --medium " + disk_name + disk_path + disk_file_format)
 
 def boot_order(VM_name):
     boot_devices = ('disk', 'dvd', 'none')
