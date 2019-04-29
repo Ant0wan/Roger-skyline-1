fccca5fb0a85e292d66b585272972642864d0665
Ant0wan <abarthel@student.42.fr>
2019-04-29 17:31:32 +0200

    push

--- src/createvm.py
+++ src/createvm.py
@@ -19,13 +19,12 @@ def configure_vm(dinfo):
     system("vboxmanage modifyvm " + dinfo['VM_name'] \
             + " --cpus " + dinfo['cpu'] \
             + " --memory " + dinfo['ram'])
-    create_disk(dinfo['VM_name'], dinfo['disk_size'], dinfo['VDI_path'], dinfo['iso_path'])
+    create_disk(dinfo['VM_name'], dinfo['disk_size'], dinfo['iso_path'], dinfo['disk_pathname'])
     boot_order(dinfo['VM_name'])
 #BUG#    system("VBoxManage modifyvm " + VM_name + " --nic2 bridged  --bridgeadapter1 " + NAT2)
 
-def create_disk(VM_name, disk_size, disk_path, iso_path):
+def create_disk(VM_name, disk_size, iso_path, disk_pathname):
     storage = ('IDE', 'SATA')
-    disk_name = 'diskdebianmini'
     disk_file_format = 'vdi'
     disk_type = 'hdd'
     for a_type in storage:
@@ -35,12 +34,12 @@ def create_disk(VM_name, disk_size, disk_path, iso_path):
             + " --storagectl " + storage[0] \
             + " --port 0 --device 0 --type dvddrive" \
             + " --medium " + iso_path)
-    system("vboxmanage createmedium disk --filename " + disk_name \
+    system("vboxmanage createmedium disk --filename " + disk_pathname \
             + " --size " + disk_size + " --format " + disk_file_format)
     system("VBoxManage storageattach " + VM_name \
             + " --storagectl " + storage[1] \
             + " --port 0 --device 0 --type " + disk_type \
-            + " --medium " + disk_name + disk_path + disk_file_format)
+            + " --medium " + disk_pathname + "." + disk_file_format)
 
 def boot_order(VM_name):
     boot_devices = ('disk', 'dvd', 'none')
