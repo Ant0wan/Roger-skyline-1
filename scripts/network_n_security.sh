35f84534c9ee47a94068af6b41a6756cd68393d5
Ant0wan <abarthel@student.42.fr>
2019-05-02 16:42:43 +0200

    ok for tmp dire

--- scripts/network_n_security.sh
+++ scripts/network_n_security.sh
@@ -14,10 +14,10 @@
 SUDO_PSSWD='root'
 
 ## Change SSH ports
-#	Server
-	echo $SUDO_PSSWD | sudo -S sed -n -i 'p;13a Port 2266'  /etc/ssh/sshd_config >/dev/null
-#	Client
-	echo $SUDO_PSSWD | sudo -S sed -n -i 'p;41a Port 2266'  /etc/ssh/ssh_config >/dev/null
+# Server
+echo $SUDO_PSSWD | sudo -S sed -n -i 'p;13a Port 2266'  /etc/ssh/sshd_config >/dev/null
+# Client
+echo $SUDO_PSSWD | sudo -S sed -n -i 'p;41a Port 2266'  /etc/ssh/ssh_config >/dev/null
 
 ## Change netmask
-	echo "root" | sudo -S sed -n -i 'p;14a \\tnetmask 255.255.255.252'  /etc/network/interfaces
+echo "root" | sudo -S sed -n -i 'p;14a \\tnetmask 255.255.255.252'  /etc/network/interfaces
