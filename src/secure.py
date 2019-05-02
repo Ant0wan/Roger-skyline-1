6649a888592c27fabf3325b30cdd58fc5c963f02
Ant0wan <abarthel@student.42.fr>
2019-05-02 17:43:04 +0200

    ok for tmp dire

--- src/secure.py
+++ src/secure.py
@@ -11,11 +11,16 @@
 #                                                                              #
 # **************************************************************************** #
 
+from time import sleep
 from os import system
+from src.start import start_vm
 
-## Start the VM
+## Secure the VM
 def secure_vm(VM_name):
     start_vm(VM_name)
+    sleep(10)
+    system("echo root | ssh -S antoine@10.11.6.42 -p 22")
+
 
 #def connect_ssh(VM_name, ip_vm, user_passwd)
 #    system("ssh ") # change port 2222
