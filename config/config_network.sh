7062a5dd1561d0ba654d0ab408cc75ca8e875e0e
Ant0wan <abarthel@student.42.fr>
2019-05-03 17:45:36 +0200

    ok for tmp dire

--- config/config_network.sh
+++ config/config_network.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 SUDO_PSSWD='root'
-##There is an error in the subject, we cannot have the same mask so we won't change nor restart service
-#echo $SUDO_PSSWD | sudo -S sed -n -i 'p;14a \\tnetmask 255.255.255.252' /etc/network/interfaces
-#echo $SUDO_PSSWD | sudo -S systemctl restart networking
+echo $SUDO_PSSWD | sudo -S sed -n -i 'p;13a \\tbroadcast 10.11.42.47' /etc/network/interfaces
+echo $SUDO_PSSWD | sudo -S sed -n -i 'p;14a \\tnetmask 255.255.255.252' /etc/network/interfaces
+echo $SUDO_PSSWD | sudo -S sed  -i '11s/allow-hotplug/auto/' /etc/network/interfaces
+echo $SUDO_PSSWD | sudo -S systemctl restart networking
