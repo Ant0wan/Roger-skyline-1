2a5d41b13b72f825f080c56d3c44e792475d153f
Ant0wan <abarthel@student.42.fr>
2019-05-03 15:58:00 +0200

    ok for tmp dire

--- src/secure.py
+++ src/secure.py
@@ -12,7 +12,7 @@
 # **************************************************************************** #
 
 from time import sleep
-from os import system, rename, environ
+from os import system
 from src.start import start_vm
 
 ## Secure the VM
@@ -22,17 +22,17 @@ def secure_vm(VM_name):
     #system("echo root | ssh antoine@10.11.6.42 -p 22")
     #ssh-keygen -t rsa -b 2048 -f config/authorized_keys -N ""
 
-#def connect_ssh(VM_name, ip_vm, user_passwd)
-#    system("ssh ") # change port 2222
-                    # block passwd connection
-                   # ssh public keys
+## Generate RSA key and move it to appropriate repo for the preseed to wget it
 #https://kb.iu.edu/d/aews
 def rsa_gen():
     system("ssh-keygen -t rsa -b 2048 -f ~/.ssh/id_rsa -N ''")
     system("mv ~/.ssh/id_rsa.pub ~/Roger-skyline-1/config/authorized_keys")
-#    rename("~/.ssh/id_rsa.pub", "~/Roger-skyline-1/config/authorized_keys")
-#    system("git add config/authorized_keys && git commit -m 'upload ssh pub key' && git push")
+    system("git add config/authorized_keys && git commit -m 'upload ssh pub key' && git push")
 
+#def connect_ssh(VM_name, ip_vm, user_passwd)
+#    system("ssh ") # change port 2222
+                    # block passwd connection
+                   # ssh public keys
 
 #print ("https://www.oracle.com/technetwork/articles/servers-storage-admin/manage-vbox-cli-2264359.html")
 ##print ("shasum < disk.vdi")
