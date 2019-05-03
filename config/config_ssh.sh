53aaef87a7b5e1374f781b822c243b7239fd44f9
Ant0wan <abarthel@student.42.fr>
2019-05-03 16:29:24 +0200

    ok for tmp dire

--- config/config_ssh.sh
+++ config/config_ssh.sh
@@ -2,3 +2,4 @@
 SUDO_PSSWD='root'
 echo $SUDO_PSSWD | sudo -S sed -n -i 'p;13a Port 2266' /etc/ssh/sshd_config >/dev/null
 echo $SUDO_PSSWD | sudo -S sed -n -i 'p;41a Port 2266' /etc/ssh/ssh_config >/dev/null
+echo $SUDO_PSSWD | sudo -S systemctl restart ssh
