71b2d814b31af8352f0a9b300c242f62e8ccb46a
Ant0wan <abarthel@student.42.fr>
2019-05-06 14:41:25 +0200

    ssh ok

--- config/config_ssh.sh
+++ config/config_ssh.sh
@@ -1,9 +1,10 @@
 #!/bin/sh
 SUDO_PSSWD='root'
+# Server
 echo $SUDO_PSSWD | sudo -S sed -n -i 'p;13a Port 2266' /etc/ssh/sshd_config
-echo $SUDO_PSSWD | sudo -S sed -n -i 'p;41a Port 2266' /etc/ssh/ssh_config
+echo $SUDO_PSSWD | sudo -S sed -n -i 'p;33a PermitRootLogin no' /etc/ssh/sshd_config
+echo $SUDO_PSSWD | sudo -S sed -n -i 'p;57a PasswordAuthentication no' /etc/ssh/sshd_config
+# Client
+#echo $SUDO_PSSWD | sudo -S sed -n -i 'p;41a Port 2266' /etc/ssh/ssh_config
+# Service restart
 echo $SUDO_PSSWD | sudo -S systemctl restart ssh
-
-# port 2222
-# block passwd connection
-# block root connection
