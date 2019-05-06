04085a35c9696cf6d6fe63e0ae133d8d17dc984e
Ant0wan <abarthel@student.42.fr>
2019-05-06 16:55:53 +0200

    ok for tmp dire

---
+++ config/config_firewall.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+# 1 - https://www.digitalocean.com/community/tutorials/how-the-iptables-firewall-works
+# 2 - https://www.digitalocean.com/community/tutorials/how-to-set-up-a-firewall-using-iptables-on-ubuntu-14-04
+# 3 - https://www.digitalocean.com/community/tutorials/iptables-essentials-common-firewall-rules-and-commands 
