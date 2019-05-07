08a0f157312594843a0302c67f2a2c7960f6841d
Ant0wan <abarthel@student.42.fr>
2019-05-07 17:58:08 +0200

    ok for tmp dire

--- config/config_ports.sh
+++ config/config_ports.sh
@@ -1,2 +1,11 @@
 #!/bin/sh
 echo $1 | sudo -S apt-get install -y portsentry
+echo $1 | sudo -S sed -i 's/BLOCK_UDP=.*/BLOCK_UDP="1"/g' /etc/portsentry/portsentry.conf
+echo $1 | sudo -S sed -i 's/BLOCK_TCP=.*/BLOCK_TCP="1"/g' /etc/portsentry/portsentry.conf
+echo $1 | sudo -S sed -i 's/BLOCK_UDP=.*/BLOCK_UDP="1"/g' /etc/portsentry/portsentry.conf
+echo $1 | sudo -S sed -i 's/#KILL_RUN_CMD=*/'\
+	'KILL_RUN_CMD=\"/sbin/iptables -I INPUT -s \$TARGET\$ -j DROP \&\& '\
+	'/sbin/iptables -I INPUT -s \$TARGET\$ -m limit --limit 3/minute '\
+	'--limit-burst 5 -j LOG --log-level debug --log-prefix '\
+	'"Portsentry: dropping: "\"/g'
+echo $1 | sudo -S systemctl restart portsentry
