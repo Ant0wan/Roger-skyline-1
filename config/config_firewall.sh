ea9c03a8db6cc7a761bd0928d4c551fec7a240b2
Ant0wan <abarthel@student.42.fr>
2019-05-07 16:50:52 +0200

    ok for tmp dire

--- config/config_firewall.sh
+++ config/config_firewall.sh
@@ -1,15 +1,13 @@
 #!/bin/sh
-SSH_PORT=$2
-DNS_PORT=$3
 echo $1 | sudo -S apt-get install -y ufw
 echo $1 | sudo -S ufw --force reset
 echo $1 | sudo -S ufw default deny incoming
 echo $1 | sudo -S ufw default deny outgoing
-echo $1 | sudo -S ufw allow $SSH_PORT/tcp
+echo $1 | sudo -S ufw allow $2/tcp
 echo $1 | sudo -S ufw allow out http
 echo $1 | sudo -S ufw allow in http
 echo $1 | sudo -S ufw allow out https
 echo $1 | sudo -S ufw allow in https
-echo $1 | sudo -S ufw allow out $DNS_PORT
+echo $1 | sudo -S ufw allow out $3
 echo $1 | sudo -S ufw logging on
 echo $1 | sudo -S ufw --force enable
