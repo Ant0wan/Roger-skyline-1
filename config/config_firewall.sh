5b23babab8477f2f8e49eab7eae1465c054d6772
Ant0wan <abarthel@student.42.fr>
2019-05-07 14:56:52 +0200

    ok for tmp dire

--- config/config_firewall.sh
+++ config/config_firewall.sh
@@ -6,4 +6,5 @@ SUDO_PSSWD='root'
 echo $SUDO_PSSWD | sudo -S iptables -P FORWARD DROP
 echo $SUDO_PSSWD | sudo -S iptables -A INPUT -p tcp --dport 2266 -j ACCEPT
 echo $SUDO_PSSWD | sudo -S iptables -A INPUT -p tcp --dport 80 -j ACCEPT
+echo $SUDO_PSSWD | sudo -S iptables -I INPUT 1 -i lo -j ACCEPT
 echo $SUDO_PSSWD | sudo -S iptables -F
