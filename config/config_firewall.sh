c459c69f4573345dd729f521dae3aa49d87dc1f6
Ant0wan <abarthel@student.42.fr>
2019-05-07 12:50:14 +0200

    ok for tmp dire

--- config/config_firewall.sh
+++ config/config_firewall.sh
@@ -2,3 +2,6 @@
 # 1 - https://www.digitalocean.com/community/tutorials/how-the-iptables-firewall-works
 # 2 - https://www.digitalocean.com/community/tutorials/how-to-set-up-a-firewall-using-iptables-on-ubuntu-14-04
 # 3 - https://www.digitalocean.com/community/tutorials/iptables-essentials-common-firewall-rules-and-commands 
+SUDO_PSSWD='root'
+echo $SUDO_PSSWD | sudo -S iptables -P FORWARD DROP
+echo $SUDO_PSSWD | sudo -S iptables -F
