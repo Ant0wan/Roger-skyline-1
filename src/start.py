4c5242ded65720078209f4ec9ac6e8aef1a9f463
Ant0wan <abarthel@student.42.fr>
2019-05-07 15:43:57 +0200

    ok for tmp dire

--- src/start.py
+++ src/start.py
@@ -6,21 +6,21 @@
 #    By: abarthel <abarthel@student.42.fr>          +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2019/02/25 18:26:40 by abarthel          #+#    #+#              #
-#    Updated: 2019/05/07 15:08:51 by abarthel         ###   ########.fr        #
+#    Updated: 2019/05/07 15:41:47 by abarthel         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
 from time import sleep
 from os import system, popen
 
-def start_vm(dinfo):
+def start_vm(VM_name):
     status = popen("vboxmanage showvminfo '" \
-            + dinfo['VM_name'] + "' | grep -c 'powered off'").read()
+            + VM_name + "' | grep -c 'powered off'").read()
     if '1' in str(status):
-        system("vboxmanage startvm " + dinfo['VM_name'] + " --type headless")
+        system("vboxmanage startvm " + VM_name + " --type headless")
 
 def ssh_vm(dinfo):
-    start_vm(dinfo)
+    start_vm(dinfo['VM_name'])
     while system("ping -c 1 -t 1 " + dinfo['ip_vm'] + " >/dev/null"):
         sleep(1)
     system("ssh -p " + dinfo['ssh_port'] + " " + \
