8af119416825f77350bb3bdd0b4f39a65a3fc7a0
Ant0wan <abarthel@student.42.fr>
2019-05-07 15:18:25 +0200

    ok for tmp dire

--- config/config_firewall.sh
+++ config/config_firewall.sh
@@ -1,17 +1,14 @@
 #!/bin/sh
 SUDO_PSSWD='root'
-# Close all pipes
+SSH_PORT='2266'
 echo $SUDO_PSSWD | sudo -S iptables -P FORWARD DROP
 echo $SUDO_PSSWD | sudo -S iptables -P INPUT DROP
 echo $SUDO_PSSWD | sudo -S iptables -P OUTPUT DROP
-# Open ports
-echo $SUDO_PSSWD | sudo -S iptables -A INPUT -p tcp --dport 2266 -j ACCEPT
+echo $SUDO_PSSWD | sudo -S iptables -t filter -A INPUT -p tcp --dport $SSH_PORT -j ACCEPT
+echo $SUDO_PSSWD | sudo -S iptables -t filter -A OUTPUT -p tcp --dport $SSH_PORT -j ACCEPT
 #echo $SUDO_PSSWD | sudo -S iptables -A INPUT -p tcp --dport 80 -j ACCEPT
-# Keep previous connections
 echo $SUDO_PSSWD | sudo -S iptables -A INPUT -m state --state RELATED,ESTABLISHED -j ACCEPT
 echo $SUDO_PSSWD | sudo -S iptables -A OUTPUT -m state --state RELATED,ESTABLISHED -j ACCEPT
-# Loopback
 echo $SUDO_PSSWD | sudo -S iptables -t filter -A INPUT -i lo -j ACCEPT
 echo $SUDO_PSSWD | sudo -S iptables -t filter -A OUTPUT -o lo -j ACCEPT
-# Load rules
 echo $SUDO_PSSWD | sudo -S iptables -F
