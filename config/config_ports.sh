0374d8ad89bdecc5f2aebbbe382bc822cbcb6837
Ant0wan <abarthel@student.42.fr>
2019-05-08 11:53:53 +0200

    ok for tmp dire

--- config/config_ports.sh
+++ config/config_ports.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 echo $1 | sudo -S apt-get install -y portsentry
-echo $1 | sudo -S sed -i 's/BLOCK_UDP="0"/BLOCK_UDP="1"/g' /etc/portsentry/portsentry.conf
-echo $1 | sudo -S sed -i 's/BLOCK_TCP="0"/BLOCK_TCP="1"/g' /etc/portsentry/portsentry.conf
-echo $1 | sudo -S sed -i "p;268a KILL_RUN_CMD=\"/sbin/iptables -I INPUT -s \$TARGET\$ -j DROP \&\& /sbin/iptables -I INPUT -s \$TARGET\$ -m limit --limit 3/minute --limit-burst 5 -j LOG --log-level debug --log-prefix 'Portsentry: dropping: '\"" /etc/portsentry/portsentry.conf
-echo $1 | sudo -S systemctl restart portsentry
+#echo $1 | sudo -S sed -i 's/BLOCK_UDP="0"/BLOCK_UDP="1"/g' /etc/portsentry/portsentry.conf
+#echo $1 | sudo -S sed -i 's/BLOCK_TCP="0"/BLOCK_TCP="1"/g' /etc/portsentry/portsentry.conf
+#echo $1 | sudo -S sed -i "p;268a KILL_RUN_CMD=\"/sbin/iptables -I INPUT -s \$TARGET\$ -j DROP \&\& /sbin/iptables -I INPUT -s \$TARGET\$ -m limit --limit 3/minute --limit-burst 5 -j LOG --log-level debug --log-prefix 'Portsentry: dropping: '\"" /etc/portsentry/portsentry.conf
+#echo $1 | sudo -S systemctl restart portsentry
