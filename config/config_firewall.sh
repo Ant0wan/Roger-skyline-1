cd5494bc413443149ee91d98d618bd6b59a780aa
Ant0wan <abarthel@student.42.fr>
2019-05-08 16:37:34 +0200

    ok for tmp dire

--- config/config_firewall.sh
+++ config/config_firewall.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+TCP_PORTS="1,7,9,11,15,70,79,80,109,110,111,119,138,139,143,512,513,514,515,540,635,1080,1524,2000,2001,4000,4001,5742,6000,6001,6667,12345,12346,20034,27665,30303,32771,32772,32773,32774,31337,40421,40425,49724,54320"
 echo $1 | sudo -S apt-get install -y ufw
 echo $1 | sudo -S ufw --force reset
 echo $1 | sudo -S ufw default deny incoming
@@ -9,6 +10,9 @@ echo $1 | sudo -S ufw allow in http
 echo $1 | sudo -S ufw allow out https
 echo $1 | sudo -S ufw allow in https
 echo $1 | sudo -S ufw allow out $3
-# Open ports for portsentry
+for i in $TCP_PORTS
+do
+	echo $1 | sudo -S ufw allow $1
+done
 echo $1 | sudo -S ufw logging on
 echo $1 | sudo -S ufw --force enable
