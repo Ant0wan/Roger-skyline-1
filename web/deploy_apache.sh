f369334e880a523020751fc83922a28c934bd341
Ant0wan <abarthel@student.42.fr>
2019-05-09 17:45:12 +0200

    ok for tmp dire

--- web/deploy_apache.sh
+++ web/deploy_apache.sh
@@ -1,9 +1,11 @@
 #!/bin/sh
 echo $1 | sudo -S apt-get install -y apache2
 echo $1 | sudo -S sed -i 's/DocumentRoot \/var\/www\/html/DocumentRoot \/home\/antoine\/www\//g' /etc/apache2/sites-available/000-default.conf
-echo $1 | sudo -S sed -n -i 'p;170a <Directory /home/antoine/www/>
-	Options FollowSymLinks
-	AllowOverride None
-	Require all granted
-</Directory>' /etc/apache2/apache2.conf
+echo $1 | sudo -S mkdir /home/antoine/www/
+echo $1 | sudo -S wget -P /home/antoine/www/ https://raw.githubusercontent.com/Ant0wan/Roger-skyline-1/master/web/www/index.html
+echo $1 | sudo -S sed -n -i "p;174a </Directory>" /etc/apache2/apache2.conf
+echo $1 | sudo -S sed -n -i "p;174a \	Require all granted" /etc/apache2/apache2.conf
+echo $1 | sudo -S sed -n -i "p;174a \	AllowOverride None" /etc/apache2/apache2.conf
+echo $1 | sudo -S sed -n -i "p;174a \	Options FollowSymLinks" /etc/apache2/apache2.conf
+echo $1 | sudo -S sed -n -i "p;174a <Directory /home/antoine/www/>" /etc/apache2/apache2.conf
 echo $1 | sudo -S service apache2 reload
