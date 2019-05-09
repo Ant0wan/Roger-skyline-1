fd7bf74669a99b2a0c31d34e9784020c43527009
Ant0wan <abarthel@student.42.fr>
2019-05-09 12:38:44 +0200

    ok for tmp dire

--- config/config_denialofservices.sh
+++ config/config_denialofservices.sh
@@ -1,2 +1,12 @@
 #!/bin/sh
 echo $1 | sudo -S apt-get install -y fail2ban
+echo '[DEFAULT]
+findfime = 3600
+bantime = 86400
+maxretry = 3
+
+[sshd]
+
+enabled = true
+port = '$2 | { cat -; } | sudo -S tee -a /etc/fail2ban/jail.d/custom.conf
+echo $1 | sudo -S systemctl restart fail2ban
