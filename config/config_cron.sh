2fc15d6be30b65b37b3b0893522e3d2944714359
Ant0wan <abarthel@student.42.fr>
2019-05-09 14:26:58 +0200

    ok for tmp dire

--- config/config_cron.sh
+++ config/config_cron.sh
@@ -4,6 +4,11 @@ echo $1 | sudo -S wget -P /etc/cron.update/ https://raw.githubusercontent.com/An
 echo $1 | sudo -S chmod 0766 /etc/cron.update/update
 echo $1 | sudo -S touch /var/log/update_script.log
 echo $1 | sudo -S chmod 0640 /var/log/update_script.log
+echo $1 | sudo -S mkdir /etc/cron.watch
+echo $1 | sudo -S wget -P /etc/cron.watch/ https://raw.githubusercontent.com/Ant0wan/Roger-skyline-1/master/config/watch_crontab
+echo $1 | sudo -S chmod 0766 /etc/cron.watch/watch_crontab
+echo $1 | sudo -S touch /var/log/watch_crontab.log
+echo $1 | sudo -S chmod 0640 /var/log/watch_crontab.log
 echo $1 | sudo -S sed -n -i 'p;14a @reboot		root	test -x /usr/sbin/anacron || ( cd / && run-parts --report /etc/cron.update )' /etc/crontab
 echo $1 | sudo -S sed -n -i 'p;14a 00 4	* * 1	root		root	test -x /usr/sbin/anacron || ( cd / && run-parts --report /etc/cron.update )' /etc/crontab
-echo $1 | sudo -S sed -n -i 'p;14a 00 0	* * *	root		root	test -x /usr/sbin/anacron || ( cd / && run-parts --report /etc/cron.update )' /etc/crontab
+echo $1 | sudo -S sed -n -i 'p;14a 00 0	* * *	root		root	test -x /usr/sbin/anacron || ( cd / && run-parts --report /etc/cron.watch )' /etc/crontab
