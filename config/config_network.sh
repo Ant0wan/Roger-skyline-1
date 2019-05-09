4611e77d6be37cb3ed23fd3423a5b1632d3d88d0
Ant0wan <abarthel@student.42.fr>
2019-05-09 17:08:22 +0200

    ok for tmp dire

--- config/config_network.sh
+++ config/config_network.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 echo $1 | sudo -S sed -n -i 'p;13a \\tbroadcast '$2 /etc/network/interfaces
 echo $1 | sudo -S sed -n -i 'p;14a \\tnetmask '$3 /etc/network/interfaces
-echo $1 | sudo -S sed  -i '11s/allow-hotplug/auto/' /etc/network/interfaces
+echo $1 | sudo -S sed -i '11s/allow-hotplug/auto/' /etc/network/interfaces
 echo $1 | sudo -S systemctl restart networking
--- web/deploy_apache.sh
+++ web/deploy_apache.sh
@@ -1,3 +1,9 @@
 #!/bin/sh
 echo $1 | sudo -S apt-get install -y apache2
 echo $1 | sudo -S sed -i 's/DocumentRoot \/var\/www\/html/DocumentRoot \/home\/antoine\/www\//g' /etc/apache2/sites-available/000-default.conf
+echo $1 | sudo -S sed -n -i 'p;170a <Directory /home/antoine/www/>
+	Options FollowSymLinks
+	AllowOverride None
+	Require all granted
+</Directory>' /etc/apache2/apache2.conf
+echo $1 | sudo -S service apache2 reload
