5c1d3990d87f07abea7fad7fec2412f11b1cc1df
Ant0wan <abarthel@student.42.fr>
2019-05-09 16:57:21 +0200

    ok for tmp dire

--- web/deploy_apache.sh
+++ web/deploy_apache.sh
@@ -1,2 +1,3 @@
 #!/bin/sh
 echo $1 | sudo -S apt-get install -y apache2
+echo $1 | sudo -S sed -i 's/DocumentRoot \/var\/www\/html/DocumentRoot \/home\/antoine\/www\//g' /etc/apache2/sites-available/000-default.conf
