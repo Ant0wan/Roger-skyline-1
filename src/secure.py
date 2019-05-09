8548ee9fdc10c214c61d2cbd7dc7b39e754e5b8a
Ant0wan <abarthel@student.42.fr>
2019-05-09 10:26:14 +0200

    ok for tmp dire

--- src/secure.py
+++ src/secure.py
@@ -6,7 +6,7 @@
 #    By: abarthel <abarthel@student.42.fr>          +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2019/02/25 18:26:40 by abarthel          #+#    #+#              #
-#    Updated: 2019/05/09 10:16:56 by abarthel         ###   ########.fr        #
+#    Updated: 2019/05/09 10:26:08 by abarthel         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
@@ -14,7 +14,8 @@ from os import system
 from time import sleep
 from src.start import start_vm
 from src.shutdown import shutdown_vm
-from src.loadscript import set_ssh, set_network, set_firewall, set_ports
+from src.loadscript import set_ssh, set_network, set_firewall, set_ports, \
+        set_grub
 
 def secure_vm(dinfo):
     sleep(5)
