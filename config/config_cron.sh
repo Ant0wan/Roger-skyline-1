8647d803661552214505f85cccb38c902882fbe5
Ant0wan <abarthel@student.42.fr>
2019-05-09 14:11:45 +0200

    ok for tmp dire

--- config/config_cron.sh
+++ config/config_cron.sh
@@ -3,3 +3,5 @@ echo $1 | sudo -S mkdir /etc/cron.update
 echo $1 | sudo -S wget -P /etc/cron.update/ https://raw.githubusercontent.com/Ant0wan/Roger-skyline-1/master/config/update
 echo $1 | sudo -S chmod 0766 /etc/cron.update/update
 echo $1 | sudo -S sed -n -i 'p;14a @reboot		root	test-x /usr/sbin/anacron || ( cd / && run-parts --report /etc/cron.update )' /etc/crontab
+echo $1 | sudo -S touch /var/log/update_script.log
+echo $1 | sudo -S chmod 0640 /var/log/update_script.log
