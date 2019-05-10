eb74a2c05b54feeb9da1d82057a2ab8895528917
Ant0wan <abarthel@student.42.fr>
2019-05-10 14:36:24 +0200

    ok for tmp dire

--- web/deploy_apache.sh
+++ web/deploy_apache.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 echo $1 | sudo -S apt-get install -y apache2
+#echo $1 | sudo -S sed -i 's/DocumentRoot \/var\/www\/html/DocumentRoot \/home\/antoine\/www\//g' /etc/apache2/sites-available/000-default.conf
 echo $1 | sudo -S mkdir /home/antoine/www/
 echo $1 | sudo -S wget -P /home/antoine/www/ https://raw.githubusercontent.com/Ant0wan/Roger-skyline-1/master/web/www/index.html
 echo $1 | sudo -S sed -n -i "p;174a </Directory>" /etc/apache2/apache2.conf
@@ -14,7 +15,7 @@ echo $1 | sudo -S openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout /e
 echo $1 | sudo -S sed -ie "s/ssl\/certs\/ssl-cert-snakeoil.pem/apache2\/ssl\/apache.crt/g" /etc/apache2/sites-available/default-ssl.conf
 echo $1 | sudo -S sed -ie "s/ssl\/private\/ssl-cert-snakeoil.key/apache2\/ssl\/apache.key/g" /etc/apache2/sites-available/default-ssl.conf
 echo $1 | sudo -S cat /etc/apache2/sites-available/default-ssl.conf | { cat -; } | sudo -S tee -a /etc/apache2/sites-available/000-default.conf
-echo $1 | sudo -S sed -n -i "p;21a \	Redirect permanent / https://"$2 /etc/apache2/sites-available/000-default.conf
 echo $1 | sudo -S sed -i 's/DocumentRoot \/var\/www\/html/DocumentRoot \/home\/antoine\/www\//g' /etc/apache2/sites-available/000-default.conf
+echo $1 | sudo -S sed -n -i "p;21a \	Redirect permanent / https://"$2 /etc/apache2/sites-available/000-default.conf
 echo $1 | sudo -S service apache2 reload
 echo $1 | sudo -S systemctl restart apache2
